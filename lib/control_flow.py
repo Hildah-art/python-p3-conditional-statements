5e62783e31fd6f5213ee9c777709fc23b851ccdb
Hildah-art <njagahildah@gmail.com>
2025-05-20 20:01:35 +0300

    worked on the hows_weather

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -10,18 +10,25 @@ def admin_login(username, password):
 
 def hows_the_weather(temperature):
     # your code here
-
+ if temperature < 40:
+       return "It's brisk out there!"
+ elif  40 <= temperature <= 65:
+       return "It's a little chilly out there!"
+ elif temperature > 85:
+       return "It's too dang hot out there!"
+ else: 
+       return "It's perfect out there!"
     
  
-    pass
+ pass
 
 def fizzbuzz(num):
     # your code here
-    
+   
     
 
     
-     pass
+    pass
 
 def calculator(operation, num1, num2):
     # your code here
