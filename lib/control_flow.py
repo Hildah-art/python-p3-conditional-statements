41b2022cdb7fdd33f7148920266ecd10989a8d9d
Hildah-art <njagahildah@gmail.com>
2025-05-20 19:57:44 +0300

    worked on the admin_login

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,31 @@
 
 def admin_login(username, password):
     # your code here
+ if (username == "admin" or username =="ADMIN") and password == "12345":
+    return "Access granted"
+ else:
+    return "Access denied" 
     pass
 
 def hows_the_weather(temperature):
     # your code here
+
+    
+ 
     pass
 
 def fizzbuzz(num):
     # your code here
-    pass
+    
+    
+
+    
+     pass
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    
+    
+
+
+     pass
